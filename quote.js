337c1b1e594582379db5cb937336bb28f58e23f5
Tushar V <tushar.vijayanagar@gmail.com>
2022-11-17 21:51:20 +0530

    added more words

--- quote.js
+++ quote.js
@@ -1,8 +1,8 @@
-let first_words = ["The magic of life", "The light of hope", "The light of your soul", "The state of being", "The depth of love", "The feeling of joy", "A content state of being", "An emotion", "The feeling of calm"]
-let second_words = ["happiness", "light", "divine",  "an ocean", "like a fluffy cloud in a sky of blue", "like a rippling pond", "like fragile glass. It can break any moment", "like a petal of a flower", "like a lilly pad floating in a pond"]
+let first_words = ["The magic of life", "The light of hope", "The light of your soul", "The state of being", "The feeling of love", "The feeling of joy", "A content state of being", "An emotion", "The feeling of calm", "A person's consiousness", "A positive mindset", "Righteousness", "A happy thought",]
+let second_words = ["happiness", "light", "divine",  "an ocean", "like a fluffy cloud in a sky of blue", "like a rippling pond", "like a petal of a flower", "like a lilly pad floating in a pond", "temporary", "a precious gem"]
 
-let first_names = ["Aiden", "Oscar", "Jenson", "Alex", "Adam", "Peter", "Peterson", "Garry", "Stephen", "Lusy","Allison", "Katie", "Sophia", "Caroline", "Elaine"]
-let last_names = ["King", "Carter", "White", "Lane", "Parker", "Bean", "Lee", "Hubbard", "Moss", "Bush", "Williams", "Wells", "Thomson", "Larson"]
+let first_names = ["Aiden", "Oscar", "Jenson", "Alex", "Adam", "Peter", "Peterson", "Garry", "Stephen", "Lusy","Allison", "Katie", "Sophia", "Caroline", "Elaine", "Nora", "Harrison", "Daniel", "joseph", "Vera", "Sally", "Jason", "Frank", "Silena", "Gloria", "Richard", "Karen","Jessica", "Leonard", "Helen"]
+let last_names = ["King", "Carter", "White", "Lane", "Bean", "Lee", "Hubbard", "Moss", "Bush", "Williams", "Wells", "Thomson", "Horton", "West", "Watson", "Herch", "Hersher", "Willsmith", "Claythorn", "McCombie", "Saunders", "Wills", "Thawn", "Farhart", "Carmichael", "Montague", "Lazrous", "Jackson", "Mclean", "Chase"]
 
 function get_random_item(items) {
   

48677c1732fc31ad2b3f2f5628f8bc49a7b3dca0
Tushar V <tushar.vijayanagar@gmail.com>
2022-11-17 20:09:21 +0530

    Changed & added better phrases

--- quote.js
+++ quote.js
@@ -1,5 +1,5 @@
-let first_words = ["The magic of life", "The light of hope", "The light of your soul", "The thought of wealth", "The abode of Heaven", "Flurry of breath", "The state of being", "The measure of love", "The depth of hate" ]
-let second_words = ["happiness", "freedom", "light", "an adventure", "like a dance", "like an eternal garden", "an ocean"]
+let first_words = ["The magic of life", "The light of hope", "The light of your soul", "The flurry of breath", "The state of being", "The depth of love", "The feeling of joy", "A content state of being", "An emotion", "The feeling of calm"]
+let second_words = ["happiness", "light", "divine",  "an ocean", "like a fluffy cloud in a sky of blue", "like a rippling pond", "like fragile glass. It can break any moment", "like a petal of a flower", "like a lilly pad floating in a pond"]
 
 let first_names = ["Aiden", "Oscar", "Jenson", "Alex", "Adam", "Peter", "Peterson", "Garry", "Stephen", "Lusy","Allison", "Katie", "Sophia", "Caroline", "Elaine"]
 let last_names = ["King", "Carter", "White", "Lane", "Parker", "Bean", "Lee", "Hubbard", "Moss", "Bush", "Williams", "Wells", "Thomson", "Larson"]
